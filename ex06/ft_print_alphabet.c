c52f1314171ec5c1f1744a04fa77a4f4b8ad404e
Beatriz <142101469+bda-mota@users.noreply.github.com>
2023-11-07 20:40:45 -0300

    Update ft_print_alphabet.c

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Create a function that displays the alphabet in lowercase, on a single line, by ascending order, starting from 
+the letter 'a'*/
 void	ft_putchar(char c);
 
 void	ft_print_alphabet(void)
