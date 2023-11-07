94ae8fb0b58a4b6ff0fb1f8b01d0b2ac5a9a4643
Beatriz <142101469+bda-mota@users.noreply.github.com>
2023-11-07 20:59:20 -0300

    Update ft_point.h

--- ex23/ft_point.h
+++ ex23/ft_point.h
@@ -10,6 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Create a file ft_point.h that compile the following main 
+
+#include "ft_point.h"
+
+void set_point(t_point *point)
+{
+	point->x = 42;
+ 	point->y = 21;
+}
+
+int main(void)
+{
+	t_point point;
+
+ 	set_point(&point);
+  	return (0);
+}
+*/
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
