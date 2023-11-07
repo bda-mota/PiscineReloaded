35665ec2376195dbd8c6533de7f129504b1a05c9
Beatriz <142101469+bda-mota@users.noreply.github.com>
2023-11-07 20:44:47 -0300

    Update ft_swap.c

--- ex10/ft_swap.c
+++ ex10/ft_swap.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Create a function that swaps the value of two integers whose addresses are entered as parameters*/
 void	ft_swap(int *a, int *b)
 {
 	int	temp;
