294f3e73b0f19bcdd5a8430cbae69efe647e89ea
Beatriz <142101469+bda-mota@users.noreply.github.com>
2023-11-07 20:51:47 -0300

    Update ft_putstr.c

--- ex15/ft_putstr.c
+++ ex15/ft_putstr.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Create a function that displays a string of characters on the standard output*/
 void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
